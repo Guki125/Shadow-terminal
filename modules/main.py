a8f3b89e979514a50794da4e956bf74d58242eaa
Guki125 <keka51090@gmail.com>
2026-03-13 15:32:29 +0200

    Fix: додав .gitignore та очистив кеш Python

--- modules/main.py
+++ modules/main.py
@@ -1,6 +1,19 @@
 import auth
 import ip_gen
 import scanner
+import ip_check
+
+def scan_results(target_ip, target_port):
+    print(f"\n[*] Target locked: {target_ip}" )
+    print(f"[*] Scanning ports")
+    for port, service in target_port.items():
+        is_open = scanner.check_port(target_ip, port)
+
+        print( ".", end = "", flush = True)
+            
+        if is_open:
+            print(f"\nPort: {port}, service: {service} is Open 🔓")
+    print()
 
 def start_shadow_terminal():
 
@@ -22,22 +35,28 @@ def start_shadow_terminal():
     print(f"\n--- Targeting system online ---")
     print(f"Protocol loaded: {list(target_port.values())}")
 
+    choice = input("\nChoose your variance for scanning: \n1. Random targets \n2. Specific targets \n> ")
+    
+    match choice:
+        case "1":
+             scan_random_targets(target_port)
+        case "2":
+            print("Wright specific target(s)...")
+            while True:
+                target_ip = input("Enter target IP (or 'done' to finish): ")
+                if target_ip.lower() == "done":
+                    return 0
+                if ip_check.is_valid_ip(target_ip):
+                    scan_results(target_ip, target_port)
+                else:
+                    print("Invalid IP address. Please try again.")
+        case _:
+            print("Invalid choice. Exiting.")
+
     for i in range(3):
         target_ip = ip_gen.generate_target()
 
-        if i == 0:
-            target_ip = "8.8.8.8"
-
-        print(f"\n[*] Target locked: {target_ip}" )
-        print(f"[*] Scanning ports")
-        for port, service in target_port.items():
-            is_open = scanner.check_port(target_ip, port)
-
-            print( ".", end = "", flush = True)
-                
-            if is_open:
-                print(f"\nPort: {port}, service: {service} is Open 🔓")
-        print()
+        scan_results(target_ip, target_port)
 
 if __name__ == "__main__":
     start_shadow_terminal()
